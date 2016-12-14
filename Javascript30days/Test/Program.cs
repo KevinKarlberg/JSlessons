d3d0a90fa5dd2d933da1a481ea8c7e1a3608e7e9
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-12-14 09:22:48 +0100

    Added projects to solution

---
+++ Javascript30days/Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hahaintename = Console.ReadLine();
+            int[] pelle = new int[10];
+            int[] kalle = new int[9];
+            int[] fuckingstooor = new int[100000];
+            WriteOutArrays(fuckingstooor, hahaintename);
+            WriteOutArrays(pelle, "käften på dig kaffe");
+           
+        }
+
+        static void WriteOutArrays(int[] a, string kaffe)
+        {
+            Console.WriteLine($"Här har du din array {kaffe} din lille fuling");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+        static void SkrivUtArray(decimal[] minArray)
+        {
+            for (int i = 0; i < minArray.Length; i++)
+            {
+                Console.WriteLine($"Värde nummer {i + 1} är: {minArray[i]}");
+            }
+        }
+    }
+}
---
+++ Lab1MVC/Lab1MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab1MVC.Startup))]
+namespace Lab1MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
